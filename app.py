44760921a2c575f60b3307261cef62b8b106cac1
perlaOlivas <59889707+perlaOlivas@users.noreply.github.com>
2020-02-04 11:24:03 -0600

    Update app.py
    university database

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
+import pymysql.cursors
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Connect to the database
+connection = pymysql.connect(host='mrbartucz.com',
+                             user='dc0758sw',
+                             password='*********',
+                             db='dc0758sw_university',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
-@app.route('/cakes')
-def cakes():
-    return 'Yummy cakes!'
+try:
+    with connection.cursor() as cursor:
+        # Select all Students
+        sql = "SELECT * FROM Student"
+        #sql = "SELECT * from Student where FIrstName = " + "'" + studentSearch + "'"
+        
+        # execute the SQL command
+        cursor.execute(sql)
+        
+        # get the results
+        @app.route('/')
+        def index():
+            return render_template('index.html')
+        
+        
+        @app.route('/uni')
+        def uni():
+            #return 'Hello, Perla'
+            row = "<br>"
+            for result in cursor:
+                row += str(result)
+                row +="<br>"
+            return (row)
+        
+      
+        # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
+        # So you must commit to save your changes. 
+        # connection.commit()
+            
+        if __name__ == '__main__':
+            app.run(debug=True, host='127.0.0.1', port='1758')
+        
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+finally:
+    connection.close()
